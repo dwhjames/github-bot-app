01fb16b872828b1a6b4e87e0689c19412b8053e9
Daniel James <dwhjames@gmail.com>
2014-12-13 00:20:45 -0500

    change logback config for log dir
    use LOG_DIR variable

--- project.clj
+++ project.clj
@@ -19,7 +19,6 @@
   :ring {:handler github-bot-app.handler/app
          :init github-bot-app.handler/init
          :destroy github-bot-app.handler/destroy}
-  :jvm-opts ["-Dcatalina.base=."]
   :profiles {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
                                   [ring-mock "0.1.5"]]}
              :uberjar {:aot :all}})
