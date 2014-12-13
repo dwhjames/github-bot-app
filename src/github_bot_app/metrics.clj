dd21fcc720f0d6fd772f9d509ca26951f1709ade
Daniel James <dwhjames@gmail.com>
2014-12-13 01:18:07 -0500

    tidy metric report map
    - make use of improvements in KeepSafe/measure library
    - sort output map

--- src/github_bot_app/metrics.clj
+++ src/github_bot_app/metrics.clj
@@ -19,7 +19,7 @@
 
 (defn metrics-map []
   (let [registry (SharedMetricRegistries/getOrCreate "github-bot-app")]
-    (-> {}
+    (-> (sorted-map)
         (into (let [gauges (into {} (.getGauges registry))]
                 (zipmap (keys gauges)
                         (map (fn [g]
@@ -35,37 +35,23 @@
         (into (let [histograms (into {} (.getHistograms registry))]
                 (zipmap (keys histograms)
                         (map (fn [h]
-                               (into {:type "histogram"
-                                      :count (measure/value h)}
+                               (into (sorted-map :type "histogram"
+                                                 :count (measure/value h))
                                      (measure/snapshot h)))
                              (vals histograms)))))
         (into (let [meters (into {} (.getMeters registry))]
                 (zipmap (keys meters)
                         (map (fn [m]
-                               (into {:type "meter"
-                                      :units "events / second"}
-                                     (measure/rates m)))
+                               (into (sorted-map :type "meter")
+                                     (measure/rates m TimeUnit/SECONDS)))
                              (vals meters)))))
-        (into (let [timers (into {} (.getTimers registry))
-                    factor (/ 1.0 (.toNanos (TimeUnit/MILLISECONDS) 1))]
+        (into (let [timers (into {} (.getTimers registry))]
                 (zipmap (keys timers)
                         (map (fn [t]
-                               (let [s (measure/snapshot t)]
-                                 (into {:type "timer"
-                                        :rate-units "calls / second"
-                                        :duration-units "milliseconds"
-                                        :count (measure/value t)
-                                        :min (* factor (:min s))
-                                        :max (* factor (:max s))
-                                        :mean (* factor (:mean s))
-                                        :std-dev (* factor (:std-dev s))
-                                        :median (* factor (:median s))
-                                        :75th-percentile (* factor (:75th-percentile s))
-                                        :95th-percentile (* factor (:95th-percentile s))
-                                        :98th-percentile (* factor (:98th-percentile s))
-                                        :99th-percentile (* factor (:99th-percentile s))
-                                        :999th-percentile (* factor (:999th-percentile s))}
-                                       (measure/rates t))))
+                               (-> (sorted-map :type "timer"
+                                               :count (measure/value t))
+                                   (into (measure/rates t TimeUnit/SECONDS))
+                                   (into (measure/snapshot t TimeUnit/MILLISECONDS))))
                              (vals timers))))))))
 
 
