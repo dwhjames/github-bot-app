5a9ce5db0bb5657742d6ba0ff42348f889f44acb
Daniel James <dwhjames@gmail.com>
2015-05-14 07:09:10 -0400

    update dependencies

--- project.clj
+++ project.clj
@@ -7,16 +7,16 @@
             :comments "same as Clojure"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [commons-codec "1.10"]
-                 [compojure "1.3.1"]
-                 [ring/ring-defaults "0.1.2"]
-                 [tentacles "0.2.6"]
-                 [cheshire "5.3.1"]
-                 [io.dropwizard.metrics/metrics-core "3.1.0"]
-                 [io.dropwizard.metrics/metrics-jvm "3.1.0"]
+                 [compojure "1.3.4"]
+                 [ring/ring-defaults "0.1.5"]
+                 [tentacles "0.3.0"]
+                 [cheshire "5.4.0"]
+                 [io.dropwizard.metrics/metrics-core "3.1.2"]
+                 [io.dropwizard.metrics/metrics-jvm "3.1.2"]
                  [measure "0.1.7"]
                  [org.clojure/tools.logging "0.3.1"]
-                 [ch.qos.logback/logback-classic "1.1.2"]]
-  :plugins [[lein-ring "0.8.13"]]
+                 [ch.qos.logback/logback-classic "1.1.3"]]
+  :plugins [[lein-ring "0.9.4"]]
   :ring {:handler github-bot-app.handler/app
          :init github-bot-app.handler/init
          :destroy github-bot-app.handler/destroy}
