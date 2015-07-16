15bfc330ff48abefd73202fa281f69b6735be05e
Daniel James <dwhjames@gmail.com>
2015-07-16 19:32:58 -0400

    tweaks to auto-label action
    - remove qa-label in auto-label
    - support unicode :+1: symbol in pattern
    - customize label removal on review done

--- src/github_bot_app/action/auto_label.clj
+++ src/github_bot_app/action/auto_label.clj
@@ -14,10 +14,7 @@
          repo (get-in payload [:repository :name])
          issue-id (get-in payload [:pull_request :number])
          base-ref (get-in payload [:pull_request :base :ref])
-         labels-to-add (concat
-                        [(get-in config [:auto-label :review-label])]
-                        (when (= base-ref "master")
-                          [(get-in config [:auto-label :qa-label])]))]
+         labels-to-add [(get-in config [:auto-label :review-start-label])]]
      (github-api-time!
       (gh-issues/add-labels
        owner repo issue-id
@@ -37,25 +34,24 @@
            issue-user (get-in payload [:issue :user :login])
            comment-user (get-in payload [:comment :user :login])
            comment-body (get-in payload [:comment :body])
-           {:keys [review-label review-done-label review-done-comment]} (:auto-label config)
+           {:keys [review-done-remove-labels review-done-add-label review-done-comment]} (:auto-label config)
            review-done-pat (re-pattern review-done-comment)]
        (when (and (not (= issue-user comment-user))
                   (re-find review-done-pat comment-body))
          (github-api-time!
           (gh-issues/add-labels
            owner repo issue-id
-           [review-done-label]
+           [review-done-add-label]
            (:auth-options config)))
          (log/info (pr-str
                     {:api-call :add-labels
                      :url (get-in payload [:issue :labels_url])
-                     :add-labels [review-done-label]}))
-         (github-api-time!
-          (gh-issues/remove-label
-           owner repo issue-id
-           review-label
-           (:auth-options config)))
-         (log/info (pr-str
+                     :add-labels [review-done-add-label]}))
+         (doseq [label review-done-remove-labels]
+          (github-api-time!
+            (gh-issues/remove-label owner repo issue-id label
+             (:auth-options config)))
+          (log/info (pr-str)
                     {:api-call :remove-label
                      :url (get-in payload [:issue :labels_url])
-                     :remove-label review-label})))))))
+                     :remove-label label})))))))
