23fed57cf818650d961e72e8fd00fa2b8275c43c
Jaromil <jaromil@dyne.org>
2017-10-19 13:45:44 +0200

    updated dependencies

--- project.clj
+++ project.clj
@@ -5,21 +5,21 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :manual
             :comments "same as Clojure"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [commons-codec "1.10"]
-                 [compojure "1.3.4"]
-                 [ring/ring-defaults "0.1.5"]
-                 [tentacles "0.3.0"]
-                 [cheshire "5.4.0"]
-                 [io.dropwizard.metrics/metrics-core "3.1.2"]
-                 [io.dropwizard.metrics/metrics-jvm "3.1.2"]
+                 [compojure "1.6.0"]
+                 [ring/ring-defaults "0.3.1"]
+                 [irresponsible/tentacles "0.6.1"]
+                 [cheshire "5.8.0"]
+                 [io.dropwizard.metrics/metrics-core "3.2.5"]
+                 [io.dropwizard.metrics/metrics-jvm "3.2.5"]
                  [measure "0.1.7"]
-                 [org.clojure/tools.logging "0.3.1"]
-                 [ch.qos.logback/logback-classic "1.1.3"]]
-  :plugins [[lein-ring "0.9.4"]]
+                 [org.clojure/tools.logging "0.4.0"]
+                 [ch.qos.logback/logback-classic "1.2.3"]]
+  :plugins [[lein-ring "0.9.7"]]
   :ring {:handler github-bot-app.handler/app
          :init github-bot-app.handler/init
          :destroy github-bot-app.handler/destroy}
   :profiles {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                                  [ring-mock "0.1.5"]]}
+                                  [ring/ring-mock "0.3.1"]]}
              :uberjar {:aot :all}})
